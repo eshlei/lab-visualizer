eca4faf4c650bc79b11acefb25d5692ca5cde529
eshlei <yenhung2@illinois.edu>
2024-11-03 10:54:31 -0600

    adjust adaptive huw window input range

--- app.js
+++ app.js
@@ -310,10 +310,10 @@ function filterByHue (e) {
     let mouseHue = Math.atan2(mouseB, mouseA);
     let mouseChroma = Math.sqrt(mouseA ** 2 + mouseB ** 2);
     let hueFilterWindow = Math.PI / 12;
-    if (mouseChroma >= 87.5 && mouseChroma < 112.5) {
-        hueFilterWindow = hueFilterWindow - 0.5 * hueFilterWindow * ((mouseChroma - 87.5) / 100 * 4) ** 2;
-    } else if (mouseChroma >= 112.5) {
-        hueFilterWindow = Math.max(0, -hueFilterWindow * 4 * (mouseChroma / 112.5 - 1) + hueFilterWindow / 2);
+    if (mouseChroma >= 100 && mouseChroma < 125) {
+        hueFilterWindow = hueFilterWindow - 0.5 * hueFilterWindow * ((mouseChroma - 100) / 100 * 4) ** 2;
+    } else if (mouseChroma >= 125) {
+        hueFilterWindow = Math.max(0, -hueFilterWindow * 4 * (mouseChroma / 125 - 1) + hueFilterWindow / 2);
     }
     // console.log(mouseChroma, Math.PI / hueFilterWindow);
     for (let i = 0; i < capturedColorsL.length; i++) {
